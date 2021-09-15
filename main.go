e567892801ade2d6a2922bee94a786e4ddf25c21
Manuel Zapf <manuel.zapf@traefik.io>
2021-09-15 23:54:34 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8010", http.HandlerFunc(basicAuth)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func basicAuth(w http.ResponseWriter, r *http.Request) {
+	username, password, ok := r.BasicAuth()
+	if ok {
+		usernameHash := sha256.Sum256([]byte(username))
+		passwordHash := sha256.Sum256([]byte(password))
+		expectedUsernameHash := sha256.Sum256([]byte("test"))
+		expectedPasswordHash := sha256.Sum256([]byte("test1"))
+
+		usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+		passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+		if usernameMatch && passwordMatch {
+			w.Write([]byte("Passed"))
+			w.WriteHeader(200)
+			return
+		}
+	}
+
+	w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+}
